cfa46b8da81f08425690a217fda1c3d009eefcc1
Ye Ji <yeji1989@me.com>
2015-02-19 01:25:32 -0500

    read from file

--- brainfuck.ml
+++ brainfuck.ml
@@ -1,9 +1,9 @@
 
 
 let _ = 
-  let lexbuf = Lexing.from_channel stdin in
+  let lexbuf = Lexing.from_channel (if Array.length Sys.argv = 1 then stdin else open_in (Array.get Sys.argv 1)) in
     let result = BrainfuckYACC.prog BrainfuckLEX.token lexbuf in
-    Bfsyntax.evalProg result ([],0,[]); ();;
+    Bfsyntax.evalProg result ([],0,[]) 
 
 
 
